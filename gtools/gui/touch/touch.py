f6f94e8d3cc6691a6395873686a149ac4fdb4281
ToyFarms <shafarrahman@gmail.com>
2026-02-22 19:17:26 +0700

    guard windows import

--- gtools/gui/touch/touch.py
+++ gtools/gui/touch/touch.py
@@ -1,9 +1,9 @@
 import sys
 
 from gtools.gui.touch.impl.null import NullTouchRouter
-from gtools.gui.touch.impl.windows.touch_win import WindowsTouchRouter
 
 if sys.platform == "win32":
+    from gtools.gui.touch.impl.windows.touch_win import WindowsTouchRouter
     TouchRouter = WindowsTouchRouter
 elif sys.platform == "darwin":
     TouchRouter = NullTouchRouter
