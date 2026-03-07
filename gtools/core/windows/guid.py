a54027726026d7ee5270b1f9e02437168220bf1f
ToyFarms <shafarrahman@gmail.com>
2026-03-07 19:13:49 +0700

    about damn time to fix this stupid error messages

--- gtools/core/windows/guid.py
+++ gtools/core/windows/guid.py
@@ -1,7 +1,22 @@
-import winreg
+import logging
+
+
+try:
+    import winreg
+
+    _has_winreg = True
+except ImportError:
+    _has_winreg = False
+
+
+logger = logging.getLogger("guid")
 
 
 def get_machine_guid() -> bytes:
-    with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Cryptography", 0, winreg.KEY_READ) as key:
-        machine_guid, _ = winreg.QueryValueEx(key, r"MachineGuid")
-        return machine_guid.encode()
+    if _has_winreg:
+        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Cryptography", 0, winreg.KEY_READ) as key:
+            machine_guid, _ = winreg.QueryValueEx(key, r"MachineGuid")
+            return machine_guid.encode()
+    else:
+        logger.warning("cannot get windows registry, because winreg does not exists")
+        return b""
